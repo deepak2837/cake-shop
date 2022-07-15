790fc6774cb3a0992511198fd119340849cf3ab9
Deepak <52418151+deepak2837@users.noreply.github.com>
2022-07-16 03:47:52 +0530

    Update settings.py

--- cakeshop/settings.py
+++ cakeshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-+qalm8!jm6ubni_^l^mml0jk=7*w)@l_32xooe4ejy(2pax76'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'cakery-webapp.herokuapp.com']
 
 
 # Application definition
